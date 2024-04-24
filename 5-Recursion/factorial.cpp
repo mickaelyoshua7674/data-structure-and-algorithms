47e537ef3ce4db680eedbd597c5935bc5d68a192
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-04-24 16:36:24 -0300

    sum of natural number and factorial with recursion

---
+++ 5-Recursion/factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int factorialRecursion(int n) {
+    if(n>1) {
+        return n*factorialRecursion(n-1);
+    } else {
+        return 1;
+    }
+}
+int factorialLoop(int n) {
+    int f=1;
+    for(int i=1;i<=n;i++) {
+        f=f*i;
+    }
+    return f;
+}
+
+int main() {
+    int n=4;
+    cout<<"Recursion: "<<factorialRecursion(n)<<endl;
+    cout<<"Loop: "<<factorialRecursion(n)<<endl;
+}
---
+++ 5-Recursion/sum-of-natural-numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int sumNaturalNumRecursion(int n) {
+    if(n>0) {
+        return sumNaturalNumRecursion(n-1)+n;
+    } else {
+        return 0;
+    }
+}
+int sumNaturalNumFormula(int n) {
+    return n*(n+1)/2;
+}
+int sumNaturalNumLoop(int n) {
+    int s=0;
+    for(int i=1;i<=n;i++) {
+        s=s+i;
+    }
+    return s;
+}
+
+int main() {
+    int i=5;
+    cout<<"Recursion: "<<sumNaturalNumRecursion(i)<<endl;
+    cout<<"Formula: "<<sumNaturalNumFormula(i)<<endl;
+    cout<<"Loop: "<<sumNaturalNumLoop(i)<<endl;
+}
