d116a49a0e27ff105016370b4c87c2ee50fca5f5
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-04-29 12:14:56 -0300

    increase array size

--- 6-Array-Representations/increase-array-size.cpp
+++ 6-Array-Representations/increase-array-size.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
 int main() {
+    int size_p=5, size_q=10;
+    int *p = new int[size_p] {3,5,7,9,1};
+    int *q = new int[size_q]; // allocate a new array with the new size
 
+    for(int i=0;i<size_p;i++) { // copy all elements from previous array to new array
+        q[i]=p[i];
+        printf("%d ", p[i]);
+    }
+    printf("\n");
+    delete[] p; // delete de address on previous array
+    p=q; // point to new array
+    q=NULL; // dereference the new pointer so there is only one pointer to new array
+    for(int i=0;i<size_p;i++) { // copy all elements from previous array to new array
+        printf("%d ", p[i]);
+    }
 }
