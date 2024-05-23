0de4e3da76f5a820f9c04da476fa480efb3e8370
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-05-23 17:02:57 -0300

    start with increase size of array and append

--- Data-Structures-ADT/Array.hpp
+++ Data-Structures-ADT/Array.hpp
@@ -13,6 +13,8 @@ private:
     int size;
     int length;
 
+    void increaseSize();
+
 public:
     Array(T a[], int l) {
         length = l;
@@ -27,11 +29,14 @@ public:
         delete[] arr;
     }
 
+    /*
+    Overloading '<<' operator to print an Array object as '[e1,e2,e3,...,en]'
+    */
     friend ostream& operator<<(ostream& out, const Array a) {
         out<<"[";
         for(int i=0; i<a.length; i++) {
             if(a.arr[i]==a.arr[a.length-1]) {
-                out<<a.arr[i]<<"]";
+                out<<a.arr[i]<<"] length="<<a.length<<" size="<<a.size;
             } else {
                 out<<a.arr[i]<<",";
             }
@@ -57,5 +62,26 @@ public:
     Array intersection(Array arr2);
     Array difference(Array arr2);
 };
+
+template <typename T>
+void Array<T>::increaseSize() {
+    size=length+2;
+    T *newArr = new T[size]; // allocate a new array with the new size
+    for(int i=0; i<length; i++) { // copy all elements from previous array to the new
+        newArr[i] = arr[i];
+    }
+    delete[] arr; // detele address of pervious array
+    arr=newArr; // point to the new array
+    newArr=NULL; // dereference the new created pointer so there is only one pointer to the new array
+    cout<<"size increased"<<endl;
+}
+
+template <typename T>
+void Array<T>::append(T value) {
+    if(length==size) {
+        increaseSize();
+    }
+    arr[length++]=value;
+}
 
 #endif
--- Data-Structures-ADT/testADT.cpp
+++ Data-Structures-ADT/testADT.cpp
@@ -3,5 +3,11 @@
 int main() {
     int b[]={1,2,3,4};
     Array<int> a{b,4};
-    cout<<a;
+    cout<<a<<endl;
+    a.append(5);
+    cout<<a<<endl;
+    a.append(6);
+    cout<<a<<endl;
+    a.append(7);
+    cout<<a<<endl;
 }
